193c65f5e9d0b9c6e3f54f6eae8fa200f0689a46
arthursereda <arthur.sereda@promova.com>
2024-07-14 23:58:46 +0300

    feat: update header link depends on auth data

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -1,13 +1,18 @@
 import { UserButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 import Link from 'next/link';
 
-const Header = () => (
-  <header className="bg-primary w-screen p-4 text-primary-foreground fixed">
-    <div className="w-2/3 m-auto flex justify-between">
-      <Link href="/">FUNNEL BUILDER</Link>
-      <UserButton />
-    </div>
-  </header>
-);
+const Header = () => {
+  const { userId } = auth();
+
+  return (
+    <header className="bg-primary w-screen p-4 text-primary-foreground fixed">
+      <div className="w-2/3 m-auto flex justify-between">
+        <Link href={userId ? '/dashboard' : '/'}>FUNNEL BUILDER</Link>
+        <UserButton />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
